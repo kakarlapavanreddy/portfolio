9dd3ddc0e2c758c12dc46bae3df92e1b052b1d62
kakarlapavankumar <kakarlapavankumar@thestackly.com>
2026-06-29 11:44:56 +0530

    initial

--- src/components/Services/Servicecard.jsx
+++ src/components/Services/Servicecard.jsx
@@ -1,48 +1,8 @@
-function ServiceCard({
-  icon: Icon,
-
-  title,
-
-  description,
-}) {
+function ServiceCard({ title, description }) {
   return (
-    <div
-      className="rounded-xl
-        bg-white
-        p-8
-        shadow-lg
-        hover:-translate-y-3
-        transition-all
-        duration-300"
-    >
-      <Icon
-        className="text-5xl
-            text-blue-600"
-      />
-
-      <h3
-        className="mt-6
-            text-2xl
-            font-bold"
-      >
-        {title}
-      </h3>
-
-      <p
-        className="mt-4
-            leading-7
-            text-gray-500"
-      >
-        {description}
-      </p>
-
-      <button
-        className="mt-6
-            font-semibold
-            text-blue-600"
-      >
-        Learn More →
-      </button>
+    <div className="p-6 shadow-md rounded-xl hover:shadow-lg transition">
+      <h3 className="text-xl font-semibold">{title}</h3>
+      <p className="text-gray-600 mt-2">{description}</p>
     </div>
   );
 }
